75c13288c10dcccd6cc0cf91d121b3f7d1a28b79
danielTLevy <danieltlevy@gmail.com>
2018-01-13 23:40:40 -0800

    main.js returns all audio database entries

--- ghostlyAPI/ghostly/ghostlyAPI/functions/__main__.js
+++ ghostlyAPI/ghostly/ghostlyAPI/functions/__main__.js
@@ -3,8 +3,57 @@
 * @param {string} name Who you're saying hello to
 * @returns {string}
 */
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let cache = null;
+
+
 module.exports = (name = 'world', context, callback) => {
+	let uri = process.env['MONGO_URI'];
+	try {
+		if (cache === null) {
+			MongoClient.connect(uri,
+				(error, db) => {
+			    	if (error) {
+			      		console.log(error['errors']);
+			      		return callback(error);
+			    	} else {
+			    		console.log("No error")
+			    	}
+			    	cache = db;
+			    	readAudio(db, callback);
+		  		});
+		} else {
+		  readAudio(cache, callback);
+		}
+	} catch (error) {
+		console.log(error);
+		return callback(error);
+	}
+};
 
-  callback(null, 'hello ${name}');
 
+const readAudio = (db, callback) => {
+	console.log(db)
+  let cursor = db.collection('audio').find();
+  let audios = [];
+  cursor.each((error, item) => {
+    console.log("iterating")
+    if (error) {
+      console.log(error);
+    }
+    console.log("1")
+    if (item === null) {
+    	console.log("2")
+      return callback(null, audios);
+    }
+    console.log("3")
+    audios.push({
+      id: item._id,
+      text: item.url,
+      completed: item.loc
+    });
+  });
 };
