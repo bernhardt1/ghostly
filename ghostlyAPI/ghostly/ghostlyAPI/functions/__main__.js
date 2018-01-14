bbfdc3cb30447fc0d45dabffb6f874d562f4d5dc
danielTLevy <danieltlevy@gmail.com>
2018-01-14 00:35:02 -0800

    Queries database to retrieve all nearby clips

--- ghostlyAPI/ghostly/ghostlyAPI/functions/__main__.js
+++ ghostlyAPI/ghostly/ghostlyAPI/functions/__main__.js
@@ -1,7 +1,6 @@
 /**
-* A basic Hello World function
-* @param {string} name Who you're saying hello to
-* @returns {string}
+* Gets all elements from database
+* @returns {array}
 */
 
 const mongodb = require('mongodb');
@@ -10,50 +9,52 @@ const MongoClient = mongodb.MongoClient;
 let cache = null;
 
 
-module.exports = (name = 'world', context, callback) => {
-	let uri = process.env['MONGO_URI'];
-	try {
-		if (cache === null) {
-			MongoClient.connect(uri,
-				(error, db) => {
-			    	if (error) {
-			      		console.log(error['errors']);
-			      		return callback(error);
-			    	} else {
-			    		console.log("No error")
-			    	}
-			    	cache = db;
-			    	readAudio(db, callback);
-		  		});
-		} else {
-		  readAudio(cache, callback);
-		}
-	} catch (error) {
-		console.log(error);
-		return callback(error);
-	}
+module.exports = (context, callback) => {
+    let uri = process.env['MONGO_URI'];
+    try {
+        if (cache === null) {
+            MongoClient.connect(uri,
+                (error, db) => {
+                    if (error) {
+                        console.log(error['errors']);
+                        return callback(error);
+                    } else {
+                        console.log("No error")
+                    }
+                    cache = db;
+                    readAudio(db, callback);
+                });
+        } else {
+          readAudio(cache, callback);
+        }
+    } catch (error) {
+        console.log(error);
+        return callback(error);
+    }
 };
 
 
 const readAudio = (db, callback) => {
-	console.log(db)
-  let cursor = db.collection('audio').find();
-  let audios = [];
-  cursor.each((error, item) => {
-    console.log("iterating")
-    if (error) {
-      console.log(error);
-    }
-    console.log("1")
-    if (item === null) {
-    	console.log("2")
-      return callback(null, audios);
-    }
-    console.log("3")
-    audios.push({
-      id: item._id,
-      text: item.url,
-      completed: item.loc
+    console.log(db)
+
+    let cursor = db.collection('audio').find();
+
+    console.log(cursor)
+    let audios = [];
+
+    cursor.each((error, item) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log("1")
+        if (item === null) {
+          return callback(null, audios);
+        }
+        audios.push({
+          id: item._id,
+          url: item.url,
+          completed: item.loc
+        });
     });
-  });
+  
 };
